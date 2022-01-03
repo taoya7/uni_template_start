da296b90972f47cf20e3fec4d94b07c59ce00434
taoya7 <5196927+taoya7@user.noreply.gitee.com>
2022-01-03 17:35:14 +0800

    feat: add git prettier

--- pages/about/about.vue
+++ pages/about/about.vue
@@ -1,19 +1,15 @@
 <template>
-	<view>
-		About
-	</view>
+  <view>
+    关于我们
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
-	}
+export default {
+  data() {
+    return {}
+  }
+}
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
